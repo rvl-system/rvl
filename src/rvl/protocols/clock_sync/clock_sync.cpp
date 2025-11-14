47693a8b246edc06393ff4948962a1452d4bfde3
Bryan Hughes <bryan@nebri.us>
2025-11-14 13:07:22 -0800

    Switched back to RBTS

--- src/rvl/protocols/clock_sync/clock_sync.cpp
+++ src/rvl/protocols/clock_sync/clock_sync.cpp
@@ -24,115 +24,99 @@ along with RVL.  If not, see <http://www.gnu.org/licenses/>.
 #include "./rvl/protocols/network_state.hpp"
 #include "./rvl/protocols/protocol.hpp"
 #include <algorithm>
-#include <iterator>
 #include <stdint.h>
+#include <string.h>
 
 namespace rvl {
 
 namespace ProtocolClockSync {
 
-#define CLOCK_SYNC_TYPE_P2P 1
-
-#define NUM_REQUESTS 4
+#define CLOCK_SYNC_PACKET_TYPE_REFERENCE_BROADCAST 1
+#define CLOCK_SYNC_PACKET_TYPE_OBSERVATION 2
+#define NUM_OBSERVATIONS_IN_SET 3
+#define NUM_NODES 240
 
 #define SYNC_ITERATION_MODULO 500
 #define SYNC_ITERATION_MODULO_MAX 1400
 
-#define SYNC_TIMEOUT (SYNC_ITERATION_MODULO_MAX - SYNC_ITERATION_MODULO)
-
-uint32_t syncTimeout = 0;
+uint32_t observations[NUM_OBSERVATIONS_IN_SET][NUM_NODES];
+uint32_t processTime = UINT32_MAX;
+uint16_t numObservations = 0;
+bool hasSentObservationThisWindow = false;
+uint16_t id = 0;
 
 /*
-Packet:
-Algorithm Type: 1 byte = 1: Point to Point (other types maybe coming soon)
+Reference Broadcast Time Synchronization, an implementation of the algorith
+described in "Fine-Grained Network Time Synchronization using Reference
+Broadcasts" by Jeremy Elson, Lewis Girod and Deborah Estrin
+
+Note: the order of packets defined here are listed in algorithmic order
+
+Parent packet:
+Type: 1 byte = 1: Reference broadcast, 2: Observation
+ID: 2 bytes = the id of the synchronization set
 Reserved: 1 byte
 
-Algorithm Type 1 Subpacket:
-Observation Number: 1 byte = The current observation
+Reference broadcast packet:
+isStartOfSet: 1 byte = 1 if this is the first broadcast in the reference set,
+  otherwise 0
 Reserved: 1 byte
-c1 (Controller Observation 1): 4 bytes = The first clock of the controller node
-r1 (Receiver Observation 1): 4 bytes = The first clock of the receiver node
-...
-cN (Controller Observation NUM_REQUESTS): 4 bytes = The fourth clock of the controller node
-rN (Receiver Observation NUM_REQUESTS): 4 bytes = The fourth clock of the receiver node
+
+Observation packet:
+clock: 4 bytes = the observed time of the reference
 */
 
-/* Algorithm Type 1
-1. The controller initializes a packet with the observation number set to 0 and
-  all clock observations set to 0
-2. The controller processes the received/initialized packet
-  2.1 The controller increments the observation number
-  2.2 The controller sets the appropriate observation slot in the packet based
-    on the observation number
-  2.3 The controller sends the packet to the receiver
-3. The receiver process the received packet
-  3.1 The receiver sets the appropriate observation slot in the packet based on
-    the observation number
-  3.2 If the observation number equals NUM_REQUESTS, then we skip to step 4
-  3.3 Otherwise, send the packet to the server and skip to step 2
-4. The receiver processes the observations
-  4.1. Calculate the periods between c1 and c2, c2 and c3, c3 and c4, r1 and r2,
-    r2 and r3, r3 and r4, and take the median / 2 as P
-  4.2. Calculate offsets On=rn-(cn+P) and find the median O
-  4.3. Update the animation clock by adding O to the current animation clock
-*/
+void processObservations() {
+  int32_t averageOffset = 0;
+  uint8_t localNode = Platform::system->getDeviceId();
+  uint8_t numObservationsProcessed = 0;
+  for (uint8_t observation = 0; observation < NUM_OBSERVATIONS_IN_SET;
+      observation++)
+  {
+    uint32_t localObservedTime = observations[observation][localNode];
 
-void init() {
-}
-
-// TODO: if two controllers are on the same network, then they both think they
-// started the synchronization, which just isn't true. I should switch this so
-// that we don't use `getDeviceMode()` to determine what to do in this function.
-void sendResponse(
-    uint8_t node, uint8_t observationNumber, uint32_t* observations) {
-  if (getDeviceMode() == DeviceMode::Controller) {
-    observationNumber++;
-    observations[(observationNumber - 1) * 2] = getAnimationClock();
-    if (observationNumber == NUM_REQUESTS) {
-      NetworkState::refreshNodeClockSyncTime(node);
-      syncTimeout = 0;
-    }
-  } else {
-    if (!NetworkState::isControllerNode(node)) {
-      return;
-    }
-    observations[(observationNumber - 1) * 2 + 1] = getAnimationClock();
-    if (observationNumber == NUM_REQUESTS) {
-      debug("Processing clock sync observation #%d", observationNumber, node);
-
-      // Step 4.1
-      int32_t periods[(NUM_REQUESTS - 1) * 2];
-      for (int i = 0; i < NUM_REQUESTS * 2 - 3; i++) {
-        periods[i] = static_cast<int32_t>(observations[i + 2]) -
-            static_cast<int32_t>(observations[i]);
+    // Sort the array so we can get the median time
+    std::sort(observations[observation], observations[observation] + NUM_NODES);
+    uint8_t head = 0;
+    while (head < NUM_NODES) {
+      if (observations[observation][head] > 0) {
+        break;
+      } else {
+        head++;
       }
-      std::sort(std::begin(periods), std::end(periods));
-      int16_t P = periods[NUM_REQUESTS - 1] / 2;
+    }
 
-      // Step 4.2
-      int32_t offsets[NUM_REQUESTS];
-      for (uint8_t i = 0; i < NUM_REQUESTS; i++) {
-        offsets[i] = static_cast<int32_t>(observations[i * 2 + 1]) -
-            static_cast<int32_t>(observations[i * 2] + P);
-      }
-      int32_t offset = offsets[(NUM_REQUESTS - 1) / 2];
+    // On rare occasions, we end up with an empty observation set. This is a bug
+    // that should be fixed, but let's also act defensively so we don't get a
+    // divide by 0 segfault and take the entire system down
+    if (head == NUM_NODES) {
+      continue;
+    }
 
-      // Step 4.3
-      setAnimationClock(getAnimationClock() - offset);
-      NetworkState::refreshLocalClockSynchronization();
-      return;
+    // Calculate the offset for this observation
+    uint32_t medianObservedTime =
+        observations[observation][head + (NUM_NODES - head) / 2];
+    averageOffset += static_cast<int32_t>(medianObservedTime) -
+        static_cast<int32_t>(localObservedTime);
+    numObservationsProcessed++;
+
+    // Reset the observation array for reuse next time
+    for (uint8_t node = head; node < NUM_NODES; node++) {
+      observations[observation][node] = 0;
     }
   }
-  debug("Sending clock sync observation #%d to source %d", observationNumber,
-      node);
-  Protocol::beginUnicastWrite(PACKET_TYPE_CLOCK_SYNC, node);
-  Platform::system->write8(CLOCK_SYNC_TYPE_P2P);
-  Platform::system->write8(0); // reserved
-  Platform::system->write8(observationNumber);
-  Platform::system->write8(0); // reserved
-  Platform::system->write(reinterpret_cast<uint8_t*>(observations),
-      NUM_REQUESTS * 2 * sizeof(uint32_t));
-  Platform::system->endWrite();
+
+  // Calculate the average offset for all observations
+  if (numObservationsProcessed > 0) {
+    averageOffset /= numObservationsProcessed;
+    debug("Updating animation clock with offset: %d", averageOffset);
+    setAnimationClock(getAnimationClock() + averageOffset);
+    NetworkState::refreshLocalClockSynchronization();
+  }
+}
+
+void init() {
+  memset(observations, 0, sizeof(observations));
 }
 
 void loop() {
@@ -151,52 +135,69 @@ void loop() {
     return;
   }
 
-  // Check if a synchronization is currently in progress
-  if (syncTimeout > 0) {
-    // Check if the synchronization has timed out
-    if (Platform::system->localClock() > syncTimeout) {
-      // If so, reset the timeout and get the next node
-      info("Clock sync with node timed out");
-      syncTimeout = 0;
-    } else {
-      // Otherwise skip
-      return;
-    }
-  }
-
-  // Get the next node to sync
-  uint8_t nextNodeToSync = NetworkState::getNextClockNode();
-  if (nextNodeToSync == 255) {
-    return;
-  }
-  syncTimeout = Platform::system->localClock() + SYNC_TIMEOUT;
-
-  // If we got here, it's time to sync the next node
-  uint32_t observations[NUM_REQUESTS * 2];
-  for (uint8_t i = 0; i < NUM_REQUESTS * 2; i++) {
-    observations[i] = 0;
-  }
-  sendResponse(nextNodeToSync, 0, observations);
+  // TODO: if we were to implement broadcast here, it would look iike:
+  /*
+  Protocol::beginBroadcastWrite(PACKET_TYPE_CLOCK_SYNC);
+  Platform::system->write8(CLOCK_SYNC_PACKET_TYPE_REFERENCE_BROADCAST);
+  Platform::system->write16(id++);
+  Platform::system->write8(0); // Reserved
+  */
 }
 
 void parsePacket(uint8_t source) {
-  debug("Parsing clock sync packet");
-  uint8_t subPacketType = Platform::system->read8();
+  uint8_t packetType = Platform::system->read8();
+  uint16_t id = Platform::system->read16();
   Platform::system->read8(); // Reserved
 
-  switch (subPacketType) {
-  case CLOCK_SYNC_TYPE_P2P: {
-    uint8_t observationNumber = Platform::system->read8();
+  switch (packetType) {
+  case CLOCK_SYNC_PACKET_TYPE_REFERENCE_BROADCAST: {
+    uint32_t observedTime = getAnimationClock();
+
+    // Send the observed time out to everyone
+    debug("Received reference broadcast with id %d at observed time %d", id,
+        observedTime);
+    Protocol::beginBroadcastWrite(PACKET_TYPE_CLOCK_SYNC);
+    Platform::system->write8(CLOCK_SYNC_PACKET_TYPE_OBSERVATION);
+    Platform::system->write16(id);
+    Platform::system->write8(0); // Reserved
+    Platform::system->write32(observedTime);
+    Platform::system->endWrite();
+
+    // Check if this the start of a set, and we need to reset our observation
+    // counter
+    bool isStartOfSet = Platform::system->read8();
     Platform::system->read8(); // reserved
-    uint32_t observations[NUM_REQUESTS * 2];
-    Platform::system->read(reinterpret_cast<uint8_t*>(observations),
-        NUM_REQUESTS * 2 * sizeof(uint32_t));
-    sendResponse(source, observationNumber, observations);
+    if (isStartOfSet == 1) {
+      numObservations = 0;
+    }
+
+    // Store this node in the observation list so that all nodes have the same
+    // observation table. This would normally be missing, since we wouldn't
+    // receive the message that was just sent
+    uint8_t observationStep = id % NUM_OBSERVATIONS_IN_SET;
+    observations[observationStep][Platform::system->getDeviceId()] =
+        observedTime;
+    break;
+  }
+
+  case CLOCK_SYNC_PACKET_TYPE_OBSERVATION: {
+    uint32_t clock = Platform::system->read32();
+    debug("Received observation from %d with clock %d", source, clock);
+    uint8_t observationStep = id % NUM_OBSERVATIONS_IN_SET;
+    observations[observationStep][source] = clock;
+    numObservations++;
+    debug("numObservations=%d numNode=%d", numObservations,
+        NetworkState::getNumNodes());
+    if (numObservations ==
+        NUM_OBSERVATIONS_IN_SET * (NetworkState::getNumNodes() - 1))
+    {
+      processObservations();
+    }
     break;
   }
 
   default: {
-    error("Received unknown clock sync subpacket type %d", subPacketType);
+    debug("Received unknown clock sync subpacket type %d", packetType);
     break;
   }
   }
